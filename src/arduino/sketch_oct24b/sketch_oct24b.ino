d4284721293226929442a1e03604411006224b65
Juan <juanbautista@ciencias.unam.mx>
2022-10-26 13:37:11 -0500

    first version with one graphic, succes :)

--- src/arduino/sketch_oct24b/sketch_oct24b.ino
+++ src/arduino/sketch_oct24b/sketch_oct24b.ino
@@ -1,3 +1,15 @@
+/**
+*Purpose: receive and print data from a sensor via an analog input.
+*testSensor.ino
+
+*@author Juan Carlos Bautista Sandoval.
+*@version 1.0.0
+**/
+
+int ldr = A0;
+int value = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -7,9 +19,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (Serial.available() > 0) {
     if (Serial.read() == 'g'){
-      srand(50);
-      int data = (rand() % 10) + 1;
-      Serial.println(data);
+      //srand(50);
+      //int data = (rand() % 10) + 1;
+      //Serial.println(data);
+      value = analogRead(ldr);
+      Serial.println(value);
     }
   }
 }
--- src/main/main_imperative.py
+++ src/main/main_imperative.py
@@ -1,22 +1,25 @@
+from time import sleep
 import tkinter as tk
-from tkinter import ttk
-import matplotlib
+from tkinter import NSEW, ttk
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 import numpy as np
 import serial
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import threading
 
-# Global variables
+# ### GLOBAL VARIABLES ### #
 
 plot_names = ['plot1', 'plot2', 'plot3']
-data_list = []
-data = np.array([])
-connection = serial.Serial('COM3', 9600)
 
-# PLOTS
+#values for graphs
+
+x_vals  = np.arange(100).tolist()
+y_vals  = np.arange(100).tolist()
+y_vals2 = np.arange(100).tolist()
+
+# figure
 
 fig = Figure()
 ax = fig.add_subplot(111)
@@ -25,11 +28,44 @@ ax.set_xlabel('Time')
 ax.set_ylabel('Data')
 ax.set_xlim(0, 100)
 ax.set_ylim(900, 1000)
-lines = ax.plot([], [])[0]
 
-# Functions
+# ### FUNCTIONS ### #
 
-def animate(i, connection):
+#get data all the time
+def get_data():
+    global y_vals
+    connection = serial.Serial('COM3',9600)
+    while True:
+        try:
+            line = connection.readline().decode('utf-8')
+            y_vals.append(float(line))
+            if len(y_vals) > 100:
+                y_vals.pop(0)
+            #print(y_vals)
+        except:
+            pass
+
+def animate(i):
+    global y_vals, connection, fig, ax, x_vals
+    #y_vals = y_vals[0:100]
+    #y_vals = y_vals[-100:]
+    #ax = plt.gcf().get_axes()
+    #ax = fig.get_axes()
+    
+    #y_vals.append(randint(0,100))
+    #print(y_vals)
+    #y_vals = y_vals[-100:]
+    #print(y_vals)
+
+    ax.cla()
+    ax.set_title('Plots')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Data')
+    ax.set_ylim(900, 1000)
+    ax.set_xlim(0, 100)
+    ax.plot(x_vals, y_vals, color='orange')
+
+    '''
     global data
     connection.write(b'g')
     input_data = connection.readline().decode('ascii')
@@ -42,23 +78,17 @@ def animate(i, connection):
     lines.set_xdata(np.arrange(0, len(data)))
     lines.set_ydata(data)
     canvas.draw()
+    '''
 
-    #ax.clear()
-    #ax.plot(data_list)
-    #ax.set_title('Plots')
-    #ax.set_xlabel('Time')
-    #ax.set_ylabel('Data')
-    #ax.set_ylim(0, 100)
-    #ax.set_xlim(900, 1000)
+# ### GUI ### #
 
-# GUI
-
-
+# main window
 root = tk.Tk()
 root.title('Real Time Plots')
 root.configure(background='blue')
 root.geometry('{0}x{1}+0+0'.format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
+# main frame
 style_main_frame = ttk.Style()
 style_main_frame.configure('MainFrame.TFrame', background='light blue')
 main_frame = ttk.Frame(root)
@@ -67,12 +97,14 @@ main_frame.columnconfigure(1, weight=4)
 main_frame.configure(style='MainFrame.TFrame')
 main_frame.pack(fill='both', expand=1, padx=10, pady=10)
 
+# side bar frame
 style_side_bar_frame = ttk.Style()
 style_side_bar_frame.configure('SideBar.TFrame', background='orange')
 side_bar_frame = ttk.Frame(main_frame)
 side_bar_frame.configure(style='SideBar.TFrame')
 side_bar_frame.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
+# title side bar frame
 title_side_bar = ttk.Label(side_bar_frame, text='Sensors')
 title_side_bar.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
@@ -80,22 +112,31 @@ for i in range(len(plot_names)):
     current_plot = ttk.Checkbutton(side_bar_frame, text=plot_names[i])
     current_plot.grid(row=i + 1, column=0, sticky=tk.NSEW, padx=10)
 
+# graphics frame
 style_graphics_frame = ttk.Style()
 style_graphics_frame.configure('Graphics.TFrame', background='orange')
 graphics_frame = ttk.Frame(main_frame)
 graphics_frame.configure(style='Graphics.TFrame')
 graphics_frame.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
+#title graphic frame
 title_graphics = ttk.Label(graphics_frame, text='Graphics')
 title_graphics.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
+#plots frame
 plots_frame = ttk.Frame(graphics_frame)
 plots_frame.grid(column=0, row=1, sticky=tk.NSEW, padx=10, pady=10)
 
-ani = animation.FuncAnimation(fig, animate, frames=100, fargs=(connection), interval=100)
+################## ANIMATION ###################
+
+thread_input_data = threading.Thread(target=get_data, args=())
+thread_input_data.start()
 
 canvas = FigureCanvasTkAgg(fig, master=plots_frame)
-canvas.get_tk_widget().pack(fill='both', expand=1)
-canvas.draw()
+canvas.get_tk_widget().grid(column=0, row=0, sticky=NSEW)
+#fig.subplots(2,1)
+
+ani = FuncAnimation(fig, animate, interval=100, blit=False)
 
 root.mainloop()
+thread_input_data.join()
