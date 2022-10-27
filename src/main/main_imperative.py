65b91440fb43f959c3028119aa60f4833e753119
Juan <juanbautista@ciencias.unam.mx>
2022-10-27 13:03:22 -0500

    2 plots real time

--- src/main/main_imperative.py
+++ src/main/main_imperative.py
@@ -1,3 +1,4 @@
+from random import randint
 from time import sleep
 import tkinter as tk
 from tkinter import NSEW, ttk
@@ -22,12 +23,19 @@ y_vals2 = np.arange(100).tolist()
 # figure
 
 fig = Figure()
-ax = fig.add_subplot(111)
+ax = fig.add_subplot(211)
 ax.set_title('Plots')
 ax.set_xlabel('Time')
 ax.set_ylabel('Data')
 ax.set_xlim(0, 100)
 ax.set_ylim(900, 1000)
+
+ax2 = fig.add_subplot(212)
+ax2.set_title('Plots')
+ax2.set_xlabel('Time')
+ax2.set_ylabel('Data')
+ax2.set_xlim(0, 100)
+ax2.set_ylim(0, 100)
 
 # ### FUNCTIONS ### #
 
@@ -41,12 +49,13 @@ def get_data():
             y_vals.append(float(line))
             if len(y_vals) > 100:
                 y_vals.pop(0)
-            #print(y_vals)
+            y_vals2.append(randint(0,100))
+            y_vals2.pop(0)
         except:
             pass
 
 def animate(i):
-    global y_vals, connection, fig, ax, x_vals
+    global y_vals, ax, x_vals, y_vals2, ax2
     #y_vals = y_vals[0:100]
     #y_vals = y_vals[-100:]
     #ax = plt.gcf().get_axes()
@@ -64,6 +73,14 @@ def animate(i):
     ax.set_ylim(900, 1000)
     ax.set_xlim(0, 100)
     ax.plot(x_vals, y_vals, color='orange')
+
+    ax2.cla()
+    ax2.set_title('Plots')
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Data')
+    ax2.set_ylim(0, 100)
+    ax2.set_xlim(0, 100)
+    ax2.plot(x_vals, y_vals2, color='purple')
 
     '''
     global data
@@ -99,18 +116,18 @@ main_frame.pack(fill='both', expand=1, padx=10, pady=10)
 
 # side bar frame
 style_side_bar_frame = ttk.Style()
-style_side_bar_frame.configure('SideBar.TFrame', background='orange')
+style_side_bar_frame.configure('SideBar.TFrame', background='red')
 side_bar_frame = ttk.Frame(main_frame)
 side_bar_frame.configure(style='SideBar.TFrame')
 side_bar_frame.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
 # title side bar frame
 title_side_bar = ttk.Label(side_bar_frame, text='Sensors')
-title_side_bar.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+title_side_bar.pack(padx=10, pady=10)
 
 for i in range(len(plot_names)):
     current_plot = ttk.Checkbutton(side_bar_frame, text=plot_names[i])
-    current_plot.grid(row=i + 1, column=0, sticky=tk.NSEW, padx=10)
+    current_plot.pack(fill=tk.X, padx=10)
 
 # graphics frame
 style_graphics_frame = ttk.Style()
@@ -121,11 +138,11 @@ graphics_frame.grid(column=1, row=0, sticky=tk.NSEW, padx=10, pady=10)
 
 #title graphic frame
 title_graphics = ttk.Label(graphics_frame, text='Graphics')
-title_graphics.grid(column=0, row=0, sticky=tk.NSEW, padx=10, pady=10)
+title_graphics.pack(padx=10, pady=10)
 
 #plots frame
 plots_frame = ttk.Frame(graphics_frame)
-plots_frame.grid(column=0, row=1, sticky=tk.NSEW, padx=10, pady=10)
+plots_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=1)
 
 ################## ANIMATION ###################
 
@@ -133,7 +150,7 @@ thread_input_data = threading.Thread(target=get_data, args=())
 thread_input_data.start()
 
 canvas = FigureCanvasTkAgg(fig, master=plots_frame)
-canvas.get_tk_widget().grid(column=0, row=0, sticky=NSEW)
+canvas.get_tk_widget().pack(fill=tk.BOTH, expand=1)
 #fig.subplots(2,1)
 
 ani = FuncAnimation(fig, animate, interval=100, blit=False)
